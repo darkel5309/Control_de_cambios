42ed9df3d97accc86678429d36e7ace344277268
Ángel Domínguez Rodríguez <angeldzrz3103@gmail.com>
2024-05-07 11:00:13 +0200

    Update ed.java
    corregir

--- ed.java
+++ ed.java
@@ -1,6 +1,6 @@
 public class Recorrer {
 
-	public void RecorrerPalabra(int palabra) {
+	public void RecorrerPalabra(String palabra) {
 		for(int i=0; i<palabra.length();i++) {
 			System.out.println(palabra.charAt(i));
 		}
